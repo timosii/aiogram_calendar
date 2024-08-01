f88d1b5af20c7ac2542cb8939411dbaaeacbf2cd
timosii <tsimashev@gmail.com>
2024-08-01 14:53:04 +0300

    corr

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiogram_calendar",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Ваши зависимости
+    ],
+)
